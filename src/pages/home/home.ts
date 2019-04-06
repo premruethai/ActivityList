d53a55f314f24b509b4f3a82b3ff52727e928b65
premruethai <pramrutai34@gmail.com>
2019-04-06 17:44:12 +0700

    ngOnInit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import {ActivityPage} from '../activity/activity';
+import {NewactivityPage}from '../newactivity/newactivity';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  addedToDoList:any[]=[];
+  tempTodo:any='';
+  todoPage = NewactivityPage;
+  constructor(public navCtrl: NavController, public navParams: NavParams ,public storage:Storage) {
+    this.storage.get('todoDetails').then(
+      (val)=>{
+        this.addedToDoList=val;
+      }
+    );
   }
+testObj = {
+  "title":"",
+  "description":""
+
+};
+
+ngOnInit():void{
+  this.storage.get(this.addedToDoList).then(
+    (val)=>{
+      this.addedToDoList=val;
+    }
+  );
+}
+ionViewWillEnter(){
+  this.storage.get('todoDetails').then((val)=>{
+    this.addedToDoList = val;
+  });
+}
+
+ionViewDidEnter(){
+  this.storage.get('todoDetails').then((val)=>{
+    this.addedToDoList = val;
+  });
+}
+
 
 }
